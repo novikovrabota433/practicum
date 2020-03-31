02553c62dfe473eeaf3b046b5513c3f3abce61b0
novikovrabota433 <32838534+novikovrabota433@users.noreply.github.com>
2020-03-31 23:51:29 +0300

    Add files via upload

---
+++ практикум/main.js
@@ -0,0 +1,374 @@
+let items = [{
+    image: 'https://pic.hata.by/imagecache/1566441698/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/037930162c06fd0dab8785b368e6e7cba8f21c1f.jpg',
+    adres: 'Проспект Газеты звезда, 10',
+    description: 'Просто притон',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1610910/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2019/12/807efb71f51f399fbfcf3d57bc3218a4f26840e8.jpg',
+    adres: 'Улица Газеты звезда, 20',
+    description: 'Уютная квартира с видом изокна на пацанский падик',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566442951/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/fef938f5e52009a4d03c608a55b6d861cc788436.jpg',
+    adres: 'Частный дом Газеты Звезда, 30',
+    description: 'Жила злая бабка',
+    room: '1',
+    square: '60',
+    float: '1',
+    price: 33000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Кто-то кашлял',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566441698/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/037930162c06fd0dab8785b368e6e7cba8f21c1f.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1610910/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2019/12/807efb71f51f399fbfcf3d57bc3218a4f26840e8.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '1',
+    square: '60',
+    float: '1',
+    price: 33000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+},{
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566441698/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/037930162c06fd0dab8785b368e6e7cba8f21c1f.jpg',
+    adres: 'prospect, 86',
+    description: 'Просто притон',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1610910/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2019/12/807efb71f51f399fbfcf3d57bc3218a4f26840e8.jpg',
+    adres: 'prospect, 86',
+    description: 'Уютная квартира с видом изокна на пацанский падик',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566442951/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/fef938f5e52009a4d03c608a55b6d861cc788436.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Жила злая бабка',
+    room: '1',
+    square: '60',
+    float: '1',
+    price: 33000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Кто-то кашлял',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566441698/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/037930162c06fd0dab8785b368e6e7cba8f21c1f.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1610910/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2019/12/807efb71f51f399fbfcf3d57bc3218a4f26840e8.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '1',
+    square: '60',
+    float: '1',
+    price: 33000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+},{
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Кто-то кашлял',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566441698/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2020/3/037930162c06fd0dab8785b368e6e7cba8f21c1f.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1610910/640x480/?image=https://pic.hata.by//images/uploaded/new_objects/2019/12/807efb71f51f399fbfcf3d57bc3218a4f26840e8.jpg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '1',
+    square: '60',
+    float: '1',
+    price: 33000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '4',
+    square: '60',
+    float: '1',
+    price: 93000,
+},{
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '2',
+    square: '60',
+    float: '1',
+    price: 63000,
+}, {
+    image: 'https://pic.hata.by/imagecache/1566438130/640x480/?image=/images/uploaded/new_objects/2020/3/41d9be3551f477a385bfced48bf9d82c921eb498.jpeg',
+    adres: 'Просект Газеты Звезда, 86',
+    description: 'Уютная квартира, расположенная в живописном месте',
+    room: '3',
+    square: '60',
+    float: '1',
+    price: 83000,
+}];
+
+const PAGINATION_GAP = 10;
+let currentPage = 1;
+
+function getVisibleItems(items) {
+    return items.slice(PAGINATION_GAP * (currentPage - 1), PAGINATION_GAP * currentPage);
+}
+
+function getCategoriesList() {
+    const categoriesMapping = items.reduce((mapping, item) => {
+        mapping[item.room] = 1;
+        return mapping;
+    }, {});
+    return Object.keys(categoriesMapping);
+}
+
+function filterByCategory(category) {
+    const filteredItems = items.filter((item) => item.room === category);
+    resetItemsList(filteredItems);
+}
+
+function filterByParams(text, category, from, to) {
+    text = text.toLowerCase().trim();
+
+    const filteredByPrice = items.filter((item) => item.price <= to && item.price >= from);
+
+    // let result = [];
+    // for (let i = 0; i < items.length; i++) {
+    //     if(items[i].price >= from && items[i].pruce <= to) {
+    //         result.push(items[i]);
+    //     }
+    // }
+
+    const filteredByText = filteredByPrice.filter(item => item.adres.toLowerCase().includes(text));
+
+
+    if (category === '') {
+        return resetItemsList(filteredByText);
+    }
+
+    const filteredByCategory = filteredByText.filter(item => item.category === category);
+    return resetItemsList(filteredByCategory);
+}
+
+function initTabs() {
+    const tabsList = document.getElementById('category-tabs');
+
+    const categoriesList = getCategoriesList();   
+
+    categoriesList.forEach((category) => {
+        const html = `
+        <div class='tab'><a href='#' data-category='${category}'>${category}</a></div>
+    `;
+        tabsList.insertAdjacentHTML('beforeend', html);
+    });
+    tabsList.insertAdjacentHTML('beforeend', 
+        `<div class='tab'><a href='#' data-category=''>All</a></div>`
+    );
+
+    tabsList.addEventListener('click', function(e) {
+        if(e.target.nodeName.toLowerCase() === 'a') {
+            e.preventDefault();
+            const categoryName = e.target.getAttribute('data-category');
+
+            if(categoryName === '') {
+                return resetItemsList(items);
+            }
+            filterByCategory(categoryName);
+        }
+    })
+}
+
+function initSearchForm() {
+    const searchButton = document.getElementById('init-search');
+    searchButton.addEventListener('click', function() {
+        const searchQuery = document.getElementById('search').value;
+        const categoryName = document.getElementById('category').value;
+        const priceFrom = Number(document.getElementById('price-from').value);
+        const priceTo = Number(document.getElementById('price-to').value);
+
+        filterByParams(searchQuery, categoryName, priceFrom, priceTo);
+    });
+
+    const categoriesList = getCategoriesList();
+    const categoriesSelect = document.getElementById('category');
+    categoriesList.forEach((category) => {
+        categoriesSelect.insertAdjacentHTML('beforeend', 
+        `<option value="${category}">${category}</option>`);
+    });
+    categoriesSelect.insertAdjacentHTML('beforeend', 
+        `<option value="" selected>All</option>`);
+}
+
+function renderPagination(items) {
+    const pages =  Math.ceil(items.length / PAGINATION_GAP);
+    const pagesList = document.getElementById('pagination');
+    pagesList.innerHTML = '';
+    if(pages < 2) {
+        return;
+    }
+
+    for (let i = 0; i < pages; i++) {
+        pagesList.insertAdjacentHTML('beforeend', 
+        `<div class='page'><a href='#' data-page='${i+1}' ${currentPage === i + 1 ? 'class="active"' : ''}>${i + 1}</a></div>`);
+    }
+
+    pagesList.addEventListener('click', function(e) {
+        if(e.target.nodeName.toLowerCase() === 'a') {
+            const pageNumber = Number(e.target.getAttribute('data-page'));
+            currentPage = pageNumber;
+            renderItemList(items);
+        }
+    });
+}
+
+function resetItemsList(items) {
+    currentPage = 1;
+    renderItemList(items);
+}
+
+function renderItemList(items) {
+
+    renderPagination(items);
+
+    const itemsContainer = document.getElementById('items-list');
+    itemsContainer.innerHTML = '';
+
+    const visibleItems = getVisibleItems(items);
+    visibleItems.forEach((item) => {
+        const html = `
+        <div class='item'>
+                <figure>
+                    <img src="${item.image}">
+                    <figcaption>
+                        <div id='div1'>
+                        <h2>${item.adres}</h2>
+                        
+                        </div>
+                        <div id='div2'>
+                        <h2>колличество комнат: ${item.room}</h2>
+                        <h2>площадь: ${item.square} м2</h2>
+                        <h2>этаж: ${item.float}</h2>
+                        <h2>цена: ${item.price}</h2>
+                        </div>
+                    </figcaption>
+                </figure>
+            </div>
+    `;
+        itemsContainer.insertAdjacentHTML('beforeend', html);
+    });
+}
+
+renderItemList(items)
+initTabs();
+initSearchForm();
+
+//<h2>${item.description}</h2>
